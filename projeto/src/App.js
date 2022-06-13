2ee74f0afe2b5956bf134e2c7f9af584bed96a17
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2022-06-13 11:04:55 -0300

    commit

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Frase from './components/Frase';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>eaee</p>
-        <Frase/>
+        <p></p>
       </div>
-     
     );
   }
 }
