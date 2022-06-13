a0db9e3be48cd5fd112287045b5859c855a55255
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2022-06-13 09:32:38 -0300

    react

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Frase from './components/Frase';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <p>eaee</p>
+        <Frase/>
       </div>
+     
     );
   }
 }
---
+++ meu-projeto/src/components/Frase.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function frase(){
+  return(
+    <div>
+      <h4>Ease</h4>
+      </div>
+  )
+}
+
+export default frase
